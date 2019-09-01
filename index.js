a1b384952391fb8de2ccebcd87843d10855b7a03
Elizabeth Peters <eliznyc@gmail.com>
2019-09-01 00:34:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -42,6 +42,6 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   if (!cardNumber) {return `Sorry, we don't have a credit card on file for you.`};
-  var cart = [];
+  cart = [];
   return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
 }
