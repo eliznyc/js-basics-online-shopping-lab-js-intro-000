3da0207f82258cc566243c5bf06e8e54f8c2898e
Elizabeth Peters <eliznyc@gmail.com>
2019-09-01 00:06:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -36,8 +36,8 @@ function total() {
 
 function removeFromCart(item) {
   for (var i = 0; i < cart.length; i++) {if (cart[i].itemName === item) {var d = i}};
-  if (cart===[]) {return `That item is not in your cart.`};
-  if (d===false) {return `That item is not in your cart.`};
+  if (cart===`[]`) {return `That item is not in your cart.`};
+  if (d===null) {return `That item is not in your cart.`};
   return cart.splice(d, 1);
 }
 
