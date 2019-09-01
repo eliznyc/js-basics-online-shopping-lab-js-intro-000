f737bb560ec59767c7b3fca3943b0338c125006a
Elizabeth Peters <eliznyc@gmail.com>
2019-09-01 00:18:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,33 +12,32 @@ function setCart(c) {
 function addToCart(item) {
   let price = Math.floor(Math.random(1) * Math.floor(99))+1 //generate random number
   let product = {itemName: `${item}`, itemPrice: price } //generate object pair
-  cart.push(product); //add item object to cart
+  cart.push(product); //add object pair to cart
   return (`${item} has been added to your cart.`);
 }
 
 function viewCart() {
   if (!cart.length) {return `Your shopping cart is empty.`};//syntax for 0 items
   if (cart.length===1) {return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`};//syntax for 1 item
-  let list=[]; //create array of all but last item
+  let list=[];
   var x = cart.length-1;
   for (var i = 0; i < x; i++) {
-  list.push(` ${cart[i].itemName} at $${cart[i].itemPrice}`)};
-  return `In your cart, you have${list}, and ${cart[x].itemName} at $${cart[x].itemPrice}.`
-}// write your code here
-
-function total() {
-  var allPrices = [];
-  for (var i = 0; i < cart.length; i++) {
-  allPrices.push(cart[i].itemPrice)};
-  return allPrices.reduce(function(a,b){return a+b}, 0);
+  list.push(` ${cart[i].itemName} at $${cart[i].itemPrice}`)}; //create array of all but last item
+  return `In your cart, you have${list}, and ${cart[x].itemName} at $${cart[x].itemPrice}.` //generate response with correct syntax for last item
 }
 
+function total() {
+  var allPrices = [];//create array of prices only
+  for (var i = 0; i < cart.length; i++) {
+  allPrices.push(cart[i].itemPrice)};
+  return allPrices.reduce(function(a,b){return a+b}, 0);//add each item in the array
+}
 
 function removeFromCart(item) {
-  for (var i = 0; i < cart.length; i++) {if (cart[i].itemName === item) {var d = i}};
-  if (cart.length===0) {return `That item is not in your cart.`};
-  if (d===null) {return `That item is not in your cart.`};
-  return cart.splice(d, 1);
+  for (var i = 0; i < cart.length; i++) {if (cart[i].itemName === item) {var d = i}}; //find index of item in array
+  if (cart.length===0) {return `That item is not in your cart.`}; //return canned response if cart is empty
+  if (d===null) {return `That item is not in your cart.`};//return canned response if item is not in cart
+  return cart.splice(d, 1);//remove item from cart and return cart
 }
 
 function placeOrder(cardNumber) {
